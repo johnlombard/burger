4c4494d5087a38649b7d0009a73d230de9d38714
John Lombard <John@Johns-MBP.fios-router.home>
2018-11-08 11:12:22 -0500

    linked to DB, local host working. Handlebars not showing anything

--- config/orm.js
+++ config/orm.js
@@ -4,54 +4,54 @@ var connection = require("../config/connection.js");
 // * `updateOne()`
 
 var orm = {
-    selectAll: function(tableInput, table) {
-      var queryString = "SELECT * FROM ?? ";
-      connection.query(queryString, [tableInput, table], function(err, result) {
-        if (err) throw err;
-        console.log(result);
-      });
-    },
-    insertOne: function(whatToSelect, table, orderCol) {
-      var queryString = "INSERT INTO " + table";
-      console.log(queryString);
-      connection.query(queryString, [whatToSelect, table, orderCol], function(err, result) {
-        if (err) throw err;
-        console.log(result);
-      });
-    },
-
-    // create: function(table, cols, vals, cb) {
-    //     var queryString = "INSERT INTO " + table;
-    
-    //     queryString += " (";
-    //     queryString += cols.toString();
-    //     queryString += ") ";
-    //     queryString += "VALUES (";
-    //     queryString += printQuestionMarks(vals.length);
-    //     queryString += ") ";
-    
-    //     console.log(queryString);
-    
-    //     connection.query(queryString, vals, function(err, result) {
-    //       if (err) {
-    //         throw err;
-    //       }
-    
-    //       cb(result);
-    //     });
-    //   },
-    findWhoHasMost: function(tableOneCol, tableTwoForeignKey, tableOne, tableTwo) {
-      var queryString =
-        "SELECT ??, COUNT(??) AS count FROM ?? LEFT JOIN ?? ON ??.??= ??.id GROUP BY ?? ORDER BY count DESC LIMIT 1";
-  
-      connection.query(
-        queryString,
-        [tableOneCol, tableOneCol, tableOne, tableTwo, tableTwo, tableTwoForeignKey, tableOne, tableOneCol],
-        function(err, result) {
-          if (err) throw err;
-          console.log(result);
+    selectAll: function(tableInput, cb) {
+      var queryString = "SELECT * FROM  " + tableInput + ";";
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
         }
-      );
+        console.log(result);
+      });
+      // cb(result);
+    },
+ 
+    insertOne: function( table, cols, vals, cb) {
+      var queryString = "INSERT INTO " + table;
+             queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+    
+        console.log(queryString);
+    
+        connection.query(queryString, vals, function(err, result) {
+          if (err) {
+            throw err;
+          }
+    
+          // cb(result);
+        });
+      },
+
+ 
+    updateOne: function(table, objColVals, condition, cb) {
+      var queryString = "UPDATE " + table;
+
+      queryString += " SET ";
+      queryString += objToSql(objColVals);
+      queryString += " WHERE ";
+      queryString += condition;
+  
+      console.log(queryString);
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+  
+        // cb(result);
+      });
     }
   };
   
